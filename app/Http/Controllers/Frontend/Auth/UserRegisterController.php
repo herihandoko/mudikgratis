347491f04db74d06944351ef5901bb5f972728a1
Heri Handoko <heri.handoko@mncgroup.com>
2025-03-02 04:29:04 +0700

    M

--- app/Http/Controllers/Frontend/Auth/UserRegisterController.php
+++ app/Http/Controllers/Frontend/Auth/UserRegisterController.php
@@ -202,7 +202,7 @@ class UserRegisterController extends Controller
     {
         $period = MudikPeriod::where('status', 'active')->orderBy('id', 'desc')->first();
         $mudikTujuan = MudikTujuan::where('code', $request->id)->where('status', 'active')->where('id_period', $period->id)->first();
-        return MudikTujuanProvinsi::with('kotax')->where('tujuan_id', $mudikTujuan->id)->where('status', 'active')->get();
+        return MudikTujuanProvinsi::with('kota')->where('tujuan_id', $mudikTujuan->id)->where('status', 'active')->get();
     }
 
     function cekStatusAktif($start_date, $end_date)
